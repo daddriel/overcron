dc72f7b17bd7f04fa7df12bcc2e88bb5f71f68cd
David Adriazola Muriel <daddriel@MacBook-Air-de-David.local>
2020-09-03 00:58:06 -0400

    add nemas

--- precache-manifest.2133323faab33434b5b4de8a58489a32.js
+++ precache-manifest.2133323faab33434b5b4de8a58489a32.js
@@ -4,7 +4,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/css/about.04c51329.css"
   },
   {
-    "revision": "41ccdd796f4cf0067fed",
+    "revision": "166235ccdb28e8f3feea",
     "url": "/css/app.9f5d6548.css"
   },
   {
@@ -28,7 +28,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/img/moodle.5a0bcf4f.svg"
   },
   {
-    "revision": "dfec1369a33ea7e895f65ce21ac6af36",
+    "revision": "a0d3e89292b956a04aa8c37ccefcccff",
     "url": "/index.html"
   },
   {
@@ -36,8 +36,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/js/about.570f71c0.js"
   },
   {
-    "revision": "41ccdd796f4cf0067fed",
-    "url": "/js/app.3304c8a8.js"
+    "revision": "166235ccdb28e8f3feea",
+    "url": "/js/app.e5d63864.js"
   },
   {
     "revision": "9e1a9d144e0d931edddb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48e05ac9093edcb9769b22e845c3fa80.js"
+  "/precache-manifest.2133323faab33434b5b4de8a58489a32.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "Overcron"});
